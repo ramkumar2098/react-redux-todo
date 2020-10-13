0a13684d85064739ce01f62cba479695d7d6f897
ramkumar2098 <ramkumar2098@protonmail.com>
2020-10-13 18:43:06 +0530

    Updates

---
+++ precache-manifest.f63afb5a726055a7877d5d812b7616aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4727c87be829c25b61f756c88384f4da",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "64e1cde66bc6ebb250e2",
+    "url": "/react-redux-todo/static/css/main.062e8152.chunk.css"
+  },
+  {
+    "revision": "d81d120594045794a5e7",
+    "url": "/react-redux-todo/static/js/2.74326786.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-todo/static/js/2.74326786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e1cde66bc6ebb250e2",
+    "url": "/react-redux-todo/static/js/main.363b984a.chunk.js"
+  },
+  {
+    "revision": "1539bfa9ef8f23f0e793",
+    "url": "/react-redux-todo/static/js/runtime-main.ebec8b13.js"
+  },
+  {
+    "revision": "63e1e59ddbb967f61972de7e40eb01d0",
+    "url": "/react-redux-todo/static/media/Delete.63e1e59d.svg"
+  },
+  {
+    "revision": "1fd297e26464c643e9fcb2ef9c8b48a5",
+    "url": "/react-redux-todo/static/media/Edit.1fd297e2.svg"
+  },
+  {
+    "revision": "29cd2954d25f18698179eb903df809c7",
+    "url": "/react-redux-todo/static/media/Save.29cd2954.svg"
+  },
+  {
+    "revision": "b99670a13f84b0fe816ee61fa02f0f8b",
+    "url": "/react-redux-todo/static/media/Theme.b99670a1.svg"
+  }
+]);
